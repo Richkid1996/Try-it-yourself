32587de34e285076e5f56722cfe380cf21a8a13f
Rico Robinson <106034410+Richkid1996@users.noreply.github.com>
2022-12-04 18:05:29 -0600

    Add files via upload

---
+++ Rain_Settings.py
@@ -0,0 +1,16 @@
+class Settings:
+	"""A class to store all settings for Alein Invasion."""
+
+	def __init__(self):
+		"""Initialize the game's settings."""
+		#Screen settings
+		self.screen_width = 1200
+		self.screen_height = 800
+		self.bg_color = (230, 230, 230)
+
+		#Raindrop settings
+		self.rain_speed = 1.5
+
+		
+		
+		
